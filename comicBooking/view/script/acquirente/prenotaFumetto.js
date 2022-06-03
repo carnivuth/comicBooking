c6dace2d2d4a07f2fd5de9a5a64a6e58c10edf49
Luca3700 <luca.torzi37@gmail.com>
2022-06-03 23:49:50 +0200

    prenotaFumetto 1

--- comicBooking/view/script/acquirente/prenotaFumetto.js
+++ comicBooking/view/script/acquirente/prenotaFumetto.js
@@ -2,14 +2,26 @@ function richiediFumettiPerPrenotazione(){
     request(uriServer + "?operazione=fumettiPerPrenotazione", myGetElementById("fumettiPerPrenotazione"));
 }
 
+fumetti
+
 function stampaFumettiPerPrenotazione(callback, result){
+
+    fumetti = result;
 
     callback.innerHTML += ("Titolo" + tab + "Numero" + "tab" + Conferma + "<br>")
 
     for (var i=0; i<result.length; i++){
         f = result[i];
         
-        callback.innerHTML += (f.titolo + tab + f.numero);
+        callback.innerHTML += (f.titolo + tab + f.numero + tab);
+        callback.innerHTML += "<button type=\"button\" value=\"" + i + "\" onclick=\"richiediFumetteriaPerPrenotazione(this.value)\">Seleziona</button> ";
+        callback.innerHTML += "<br";
     }
-    <button type="button" onclick="richiediFumetteriaPerPrenotazione(this)">Click Me!</button> 
+}
+
+function richiediFumetteriaPerPrenotazione(numeroFumetto){
+    numeroFumetto = parseInt(numeroFumetto);
+    f = fumetti[numeroFumetto];
+    jsonFumetto = JSON.stringify(f);
+    request(uriServer + "?operazione=fumetteriePerPrenotazione&fumetto="+jsonFumetto, myGetElementById("fumettiPerPrenotazione"));
 }
