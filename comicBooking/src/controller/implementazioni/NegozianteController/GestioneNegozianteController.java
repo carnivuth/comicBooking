7510443195a8901dd9efdd9c1d6b356ba0f9da62
Turixen <giorgio.mastrotucci@gmail.com>
2022-06-06 23:42:45 +0200

    restituizione catalogo

--- comicBooking/src/controller/implementazioni/NegozianteController/GestioneNegozianteController.java
+++ comicBooking/src/controller/implementazioni/NegozianteController/GestioneNegozianteController.java
@@ -11,9 +11,11 @@ import javax.servlet.http.HttpServletResponse;
 import com.google.gson.Gson;
 
 import controller.datastore.DataStoreControllerFactory;
+import controller.interfacce.iNegoziante.IGestioneCatalogo;
 import controller.interfacce.iNegoziante.IGestioneNegoziante;
 import controller.interfacce.iPercistance.IDataStore;
 import controller.interfacce.iPercistance.IDataStoreFactory;
+import model.Wrapper;
 
 public class GestioneNegozianteController extends HttpServlet implements IGestioneNegoziante {
 
@@ -33,7 +35,15 @@ public class GestioneNegozianteController extends HttpServlet implements IGestio
         String operazione=req.getParameter("operazione");
         switch (operazione) {
             case "catalogo":
-                
+                IGestioneCatalogo c=new GestioneCatalogoController();
+                String username=(String) req.getSession().getAttribute("username"); 
+               
+                c.getCatalogo (dataStore.getNegoziante(username));
+                Wrapper w= new Wrapper();
+                w.setResult(c.getCatalogo (dataStore.getNegoziante(username)));
+                w.setOperazione("catalogo");
+                           
+                resp.getWriter().println(g.toJson(w));
                 break;
 
             case "changePrenotabile":
