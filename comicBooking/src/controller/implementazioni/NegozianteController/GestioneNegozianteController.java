ad4ca995e1b658212c3f0a67f2beb3d7888e6b49
carnivuth <matti200042@gmail.com>
2022-06-07 21:58:27 +0200

    refactor eliminazione commenti

--- comicBooking/src/controller/implementazioni/NegozianteController/GestioneNegozianteController.java
+++ comicBooking/src/controller/implementazioni/NegozianteController/GestioneNegozianteController.java
@@ -74,7 +74,7 @@ public class GestioneNegozianteController extends HttpServlet implements IGestio
 
     @Override
     public String getUsername() {
-        // TODO Auto-generated method stub
+        
         return null;
     }
     
