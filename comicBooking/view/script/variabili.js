1d1fae6c20d8d32ccf415f7d95ee20fd74590051
Luca3700 <luca.torzi37@gmail.com>
2022-06-10 22:00:46 +0200

    messi link alle home page in una variabile

--- comicBooking/view/script/variabili.js
+++ comicBooking/view/script/variabili.js
@@ -1,8 +1,21 @@
 
-uriServer = "http://localhost:8080/fumetto";
+uriServer = "http://localhost:8080/fumetto/";
+uriRegistrazione = uriServer + registrazione.jsp;
+uriHomeNegoziante = uriServer + HomeGestioneNegoziante.html;
+uriHomeAcquirente = uriServer + HomeGestioneAcquirente.html;
+
 tab = "\t"
+
 
 function getUsername(){
     cookie = document.cookie.split("=")[1]; 
     return cookie;
+}
+
+function gotoHomeAcquirente(){
+    window.location.href = uriHomeAcquirente;
+}
+
+function gotoHomeNegoziante(){
+    window.location.href = uriHomeNegoziante;
 }
